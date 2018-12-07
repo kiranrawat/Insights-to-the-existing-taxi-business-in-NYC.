aacc0ae56170b2b9d5b49682d816ea76e78386c1
Vaishnavi Malhotra <vma19@sfu.ca>
2018-12-06 20:49:07 -0800

    seperated files for driver based analysis month and week

--- driver_weekly.py
+++ driver_weekly.py
@@ -20,15 +20,10 @@ def main(input_data, input_fare, output):
     # main logic starts here
     tripdata = spark.read.option("header","true").csv(input_data)
     data_df = tripdata.filter((tripdata['trip_distance']>0) & (tripdata['pickup_longitude']!=0) & (tripdata['pickup_longitude']<180.00) & (tripdata['pickup_longitude']>-180) &  (tripdata['pickup_latitude']!=0) & (tripdata['pickup_latitude']<90.00) & (tripdata['pickup_latitude']>-90.00) & (tripdata['dropoff_longitude']!=0) & (tripdata['dropoff_latitude']!=0) & (tripdata['dropoff_latitude']<90.00) & (tripdata['dropoff_latitude']>-90.00) & (tripdata['dropoff_longitude']<180.00) & (tripdata['dropoff_longitude']>-180)).drop('store_and_fwd_flag')
-    #data_df.show()
     fare_df = spark.read.option("header","true").csv(input_fare)
-    #fare_df.show()
-    '''
-        geolocator = Nominatim(user_agent="nyctaxi")
-        location = geolocator.reverse("40.741245, -73.978775")
-        print(location.raw)'''
     joined_df = data_df.join(fare_df, [data_df['medallion']==fare_df['medallion'], data_df['hack_license']==fare_df[' hack_license'], data_df['pickup_datetime']==fare_df[' pickup_datetime']]).select(data_df['*'],fare_df[' total_amount'].alias('total_amount'), (sf.concat(data_df['pickup_latitude'], sf.lit(","), data_df['pickup_longitude'])).alias('pickupLoc'), (sf.concat(data_df['dropoff_latitude'], sf.lit(","), data_df['dropoff_longitude'])).alias('dropoffLoc'))
-    joined_df.write.format('json').save(output)
+    weekly_summary = joined_df.groupBy('medallion', 'hack_license', sf.weekofyear('pickup_datetime').alias('p_week')).agg(sf.sum('trip_distance').alias('sum_dist'), sf.sum('total_amount').alias('sum_amount'), sf.sum('trip_time_in_secs').alias('sum_trip'), sf.sum('passenger_count').alias('passenger_count'))
+    weekly_summary.write.format('json').save(output)
 
 if __name__ == '__main__':
     input_data = sys.argv[1]
